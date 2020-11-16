9e05713748befa2898e7cc156c9a42245c6f86a9
ETechDev <Edrag@users.noreply.github.com>
2020-11-16 16:13:24 +0200

    fixed table scroll on home

--- src/components/home/homelast5.js
+++ src/components/home/homelast5.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './homelast5.css';
 
 
 
@@ -34,55 +35,57 @@ class HomeLast5 extends React.Component {
             <div className="tables">
                 <h2>Last 5 Entries</h2>
                 <span className='fillertext'>(Click to Modify)</span>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Date and Time</th>
-                            <th>Berry Type</th>
-                            <th>Block</th>
-                            <th>Variety</th>
-                            <th>Grade</th>
-                            <th>Batch Num</th>
-                            <th>Gross Weight</th>
-                            <th># Crates</th>
-                            <th>Crate Weight</th>
-                            <th>Nett Weight</th>
-                            <th>Team</th>
-                            <th>Temperature</th>
-                            <th>Brix</th>
-                            <th>g/Berry</th>
-                            <th>Colour</th>
-                            <th>Feel</th>
-                            <th>Comments</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.props.berryRecords5.map(item=>
-                            <tr key={item.EntryId}>
-                                <td>{item.DateTime}</td>
-                                <td>{item.BerryType}</td>
-                                <td>{item.BlockName}</td>
-                                <td>{item.VarietyName}</td>
-                                <td>{item.BerryGrade}</td>
-                                <td>{item.BatchNum}</td>
-                                <td>{item.GrossWeight}</td>
-                                <td>{item.NumOfCrates}</td>
-                                <td>{item.IndivCrateWeight}</td>
-                                <td>{item.NettWeight}</td>
-                                <td>{item.Team}</td>
-                                <td>{item.Temperature}</td>
-                                <td>{item.Brix}</td>
-                                <td>{item.GramPerBerry}</td>
-                                <td>{item.Colour}</td>
-                                <td>{item.Feel}</td>
-                                <td>{item.Comments}</td>
-                                <td className='actionlast5row' id={item.EntryId} onClick={e=>this.modifyEntry(e)}>Edit</td>
-                                <td className='actionlast5row' id={item.EntryId} onClick={e=>this.deleteEntry(e)}>Delete</td>
+                <div id = "last5TableWrap" >
+                    <table>
+                        <thead id ="last5Head">
+                            <tr>
+                                <th>Date and Time</th>
+                                <th>Berry Type</th>
+                                <th>Block</th>
+                                <th>Variety</th>
+                                <th>Grade</th>
+                                <th>Batch Num</th>
+                                <th>Gross Weight</th>
+                                <th># Crates</th>
+                                <th>Crate Weight</th>
+                                <th>Nett Weight</th>
+                                <th>Team</th>
+                                <th>Temperature</th>
+                                <th>Brix</th>
+                                <th>g/Berry</th>
+                                <th>Colour</th>
+                                <th>Feel</th>
+                                <th>Comments</th>
+                                <th></th>
                             </tr>
-                        )}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>                                
+                            {this.props.berryRecords5.map(item=>
+                                <tr key={item.EntryId}>
+                                    <td>{item.DateTime}</td>
+                                    <td>{item.BerryType}</td>
+                                    <td>{item.BlockName}</td>
+                                    <td>{item.VarietyName}</td>
+                                    <td>{item.BerryGrade}</td>
+                                    <td>{item.BatchNum}</td>
+                                    <td>{item.GrossWeight}</td>
+                                    <td>{item.NumOfCrates}</td>
+                                    <td>{item.IndivCrateWeight}</td>
+                                    <td>{item.NettWeight}</td>
+                                    <td>{item.Team}</td>
+                                    <td>{item.Temperature}</td>
+                                    <td>{item.Brix}</td>
+                                    <td>{item.GramPerBerry}</td>
+                                    <td>{item.Colour}</td>
+                                    <td>{item.Feel}</td>
+                                    <td>{item.Comments}</td>
+                                    <td className='actionlast5row' id={item.EntryId} onClick={e=>this.modifyEntry(e)}>Edit</td>
+                                    <td className='actionlast5row' id={item.EntryId} onClick={e=>this.deleteEntry(e)}>Delete</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         )
     };
